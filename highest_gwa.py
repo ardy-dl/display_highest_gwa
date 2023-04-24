ff0905fd73ea80028c93e3e590ef5803d51a9616
unknown <rosedenisejdeleon@iskolarngbayan.pup.edu.ph>
2023-04-24 23:29:40 +0800

    Pseudocode

---
+++ highest_gwa.py
@@ -0,0 +1,8 @@
+# Write a Python program that read a file containing the name of 20 students together with their GWA. 
+# The program will outputs the name of the student who got the highest GWA (including the GWA).
+
+# name a list
+# open classrecord.txt (read)
+# seperate name and gwa by using list
+# find the highest gwa
+# display name and gwa
