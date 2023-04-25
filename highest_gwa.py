6a96304ed1617ef708b9824ad96c1f2060bf63cc
unknown <rosedenisejdeleon@iskolarngbayan.pup.edu.ph>
2023-04-25 18:38:12 +0800

    Program finally done!

--- highest_gwa.py
+++ highest_gwa.py
@@ -13,10 +13,20 @@ with open("class_record.txt") as ref_file:
     highest_GWA = min(master_list, key=lambda x: x["GWA"]) 
     # find all the students who got the highest grade
     best_students = [student for student in master_list if student["GWA"] == highest_GWA["GWA"]]
+
+    # for designing
+    from termcolor import colored
+    from pyfiglet import Figlet
+
     # display name and gwa 
     if len(best_students) == 1:
-        print(highest_GWA["name"], "is the highest with the GWA of", highest_GWA["GWA"])
+        f = Figlet(font = "banner3-D")
+        design_1 = colored(highest_GWA["name"], "is the highest with the GWA of", highest_GWA["GWA"], color= "green", attrs = ["italic"])
+        print(design_1)
     else:
-        print("Here are the students who got the highest GWA:\n", highest_GWA["GWA"])
+        f = Figlet(font = "banner3-D")
+        design_2 = colored("Here are the students who got the highest GWA of " + str(highest_GWA["GWA"]), "yellow", attrs = ["bold"])
+        print(design_2)
         for students in best_students:
-            print(students["name"], "-", students["GWA"])
+            design_3 = colored(students["name"] + " - " + str(students["GWA"]), "blue", attrs = ["underline"])
+            print(design_3)
